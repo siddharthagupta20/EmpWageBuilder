6bcdb9809a26a7ed9a4e70f5e9a2fb1f6fe56db6
siddharthagupta20 <siddharthagupta20@gmail.com>
2020-09-24 00:58:34 +0530

    added to UC5

--- src/com/wages/EmpWageBuilderUC.java
+++ src/com/wages/EmpWageBuilderUC.java
@@ -6,11 +6,15 @@ public class EmpWageBuilderUC {
 	public static final int IS_PART_TIME=1;
 	public static final int IS_FULL_TIME=2;
 	public static final int EMP_RATE_PER_HOUR=20;
+	public static final int NUM_OF_WORKING_DAYS=2;	
 	
 	public static void main(String [] args) {
 		
 		int empHrs=0;
 		int empWage=0;
+		int totalEmpWages=0;
+		int day=0;
+		while(day!=NUM_OF_WORKING_DAYS) {
 		int empCheck =(int)Math.floor(Math.random()*10) %3;
 		switch(empCheck) {
 		case IS_PART_TIME:
@@ -25,6 +29,11 @@ public class EmpWageBuilderUC {
 			empHrs=0;
 			}
 		empWage=empHrs* EMP_RATE_PER_HOUR;
+		totalEmpWages+=empWage;
 		System.out.println("Emp Wage: "+ empWage);
+		day++;
+		}
+		System.out.println("Total Emp Wages: "+totalEmpWages);
+		
 	}
 }
