240b852de3897ef044871f7e9ac684f69ec08668
siddharthagupta20 <siddharthagupta20@gmail.com>
2020-09-24 00:39:18 +0530

    added to UC3

--- src/com/wages/EmpWageBuilderUC.java
+++ src/com/wages/EmpWageBuilderUC.java
@@ -4,15 +4,19 @@ package com.wages;
 public class EmpWageBuilderUC {
 	
 	public static void main(String [] args) {
-		int IS_FULL_TIME=1;
+		int IS_FULL_TIME=2;
+		int IS_PART_TIME=1;
 		int EMP_RATE_PER_HOUR=20;
 		
 		int empHrs=0;
 		int empWage=0;
-		double empCheck =Math.floor (Math.random()*10%2);
-		if(IS_FULL_TIME==empCheck) {
-			empHrs=8;
+		double empCheck =Math.floor (Math.random()*10%3);
+		if(IS_PART_TIME==empCheck) {
+			empHrs=4;
 			
+		}
+		else if(empCheck==IS_FULL_TIME) {
+			empHrs=8;
 		}
 		else {
 			empHrs=0;
