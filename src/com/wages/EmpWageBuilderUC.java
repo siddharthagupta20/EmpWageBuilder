1e16f5aad191e2fcefcfb58ecea4959bcc5c2ce7
siddharthagupta20 <siddharthagupta20@gmail.com>
2020-09-23 23:38:25 +0530

    added to master

---
+++ src/com/wages/EmpWageBuilderUC.java
@@ -0,0 +1,14 @@
+package com.wages;
+
+
+public class EmpWageBuilderUC {
+	static int IS_FULL_TIME=1;
+	public static void main(String [] args) {
+		double empCheck =Math.floor (Math.random()*10%2);
+		if(IS_FULL_TIME==empCheck) {
+			System.out.println("The employee is present.");
+		}
+		else
+			System.out.println("The employee is absent.");
+	}
+}
